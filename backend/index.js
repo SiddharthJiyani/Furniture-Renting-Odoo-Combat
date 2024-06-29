2d766b280b8f17e9e6afa534e0879804491d26d8
Rajat Agrawal <agrawalrajat357@gmail.com>
2024-06-29 16:24:28 +0530

    Add files via upload

--- backend/index.js
+++ backend/index.js
@@ -2,9 +2,9 @@ const express = require("express");
 require("dotenv").config();
 const app = express();
 const cors = require("cors");
-const bookingRouter = require('./routes/bookings');
+const bookingRoutes = require('./routes/bookings');
 const port = process.env.PORT || 4000 ;
-
+const authenticateToken = require('./middleware/authMiddleware');
 // Middleware
 app.use(express.json()); 
 
@@ -18,16 +18,27 @@ app.use(
 
 // Routes
 const userRoutes = require("./routes/user");
+const furnitureRoutes = require("./routes/furniture");
 app.use("/api/auth", userRoutes);
+app.use("/api/furniture",furnitureRoutes)
 
 // Database connection
 const db = require("./config/database");
 db.connectDB();
 
 // Booking Router
-app.use('/api/bookings', bookingRouter);
+app.use('/api/bookings', authenticateToken, bookingRoutes);
 
 // Start the server
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    if (req.method === 'OPTIONS') {
+      res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+      return res.status(200).json({});
+    }
+    next();
+  });
